f4649211d93008560eef05ec72a1c2fefeff41e2
unknown <d.nikolov7654@gmail.com>
2023-04-21 21:46:35 +0300

    commit so I can go on pc

--- server/server.go
+++ server/server.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+    "time"
+    socketio "github.com/googollee/go-socket.io"
 )
 
 func getIndex(w http.ResponseWriter, r *http.Request) {
@@ -48,6 +51,13 @@ func getRemind(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "../client/html/reminder.html")
 }
 
+func handleEsp(io socketio.Server, acc string) {
+    io.OnEvent("/" + acc, "newReminder", func (socket socketio.Conn) {
+        log.Println("paca")
+    })
+
+}
+
 func main() {
 	static := http.FileServer(http.Dir("../client/static"))
 	http.Handle("/", static);
@@ -55,6 +65,38 @@ func main() {
 	http.HandleFunc("/home", getIndex) 
 	http.HandleFunc("/auth", getAuth) 
 	http.HandleFunc("/remind", getRemind) 
+    //http.HandleFunc("/esp", connEsp)
+
+    io, err := socketio.NewServer(nil);
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    /*
+    io.OnEvent("/", "connection", func (socket socketio.Socket) {
+        log.Println("a device connected")
+
+        // socket.On("getNotifications", func ()
+        socket.OnEvent("/", "connectAccount" func (
+    })
+    */
+
+    io.OnConnect("/", func (socket socketio.Conn) error {
+        log.Println("a device connected: ", socket.ID()) 
+        return nil
+    })
+
+    io.OnError("/", func (socket socketio.Conn, e error) {
+        log.Println("encountered error: ", e)
+    })
+
+    io.OnDisconnect("/", func (socket socketio.Conn, reason string) {
+        log.Println("device disconnected: ", socket.ID())
+    })
+
+    io.OnEvent("/", "connectAccount", func (socket socketio.Conn, acc string) {
+        
+    });
 
 	fmt.Printf("Starting server at port 80\n")
     if err := http.ListenAndServe(":80", nil); err != nil {
